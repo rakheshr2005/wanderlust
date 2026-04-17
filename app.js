38e47e2f972f1b730fb0bf8a560b26c22823b582
Rakhesh R <rrakhesh2005@gmail.com>
2026-04-17 11:20:57 +0530

    styling added for pages

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const mongoose = require("mongoose");
 const Listing = require("./MODELS/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"VIEWS"));
 
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.engine('ejs',ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -78,7 +81,7 @@ app.get("/listings/:id/edit",async (req,res)=>{
 //update Route
 app.put("/listings/:id",async (req,res)=>{
     let {id} = req.params;
-    await Listing.findByIdAndUpdate(id, {...req.body.listin});
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
     res.redirect(`/listings/${id}`);
 })
 
